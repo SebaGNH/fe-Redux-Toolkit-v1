f0e6c14a0edb1cb4d2189db513b41ca66e4a8866
Sebastian Gonzalez <seba.gnh@gmail.com>
2022-12-02 22:04:37 -0300

    se muestra el estado de los componentes y se modifican con redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './store/slices/counter/counterSlice'
+
 
 const App = () => {
+
+  //const counter = useSelector((state) => state.counter.value)
+  const counter = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
   return (
-    <div>App</div>
+    <> 
+    <header>
+      <h1>Counter {counter}</h1>
+    </header>
+    <main>
+
+      <button 
+        className="btn btn-primary me-3"
+        onClick={() => dispatch(increment())}
+        >sumar
+      </button>
+      <button 
+        className="btn btn-info"
+        onClick={() => dispatch(decrement())}
+        >restar
+      </button>
+    </main>
+    
+    </>
   )
 }
 
