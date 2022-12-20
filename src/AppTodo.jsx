fdd26cc899f1080e0c389f98ef3a7a730d23948a
Sebastian Gonzalez <seba.gnh@gmail.com>
2022-12-20 20:44:28 -0300

    se crea componente todosApp

---
+++ src/AppTodo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const AppTodo = () => {
+  return (
+    <div className='container'>
+      <div className="row mt-3">
+        <h1>Todos - RTK Query</h1>
+        <hr />
+        <h4>isLoading</h4>
+        <pre>...</pre>
+        <button className='btn btn-primary col-2'>Next Todo</button>
+      </div>
+    </div>
+  )
+}
+
+export default AppTodo
--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,15 @@ import './index.css'
 import { store } from './store/store'
 import { Provider } from 'react-redux'
 import AppPokemon from './AppPokemon'
+import AppTodo from './AppTodo'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <App /> */}
-      <AppPokemon />
+      {/* <AppPokemon /> */}
+      <AppTodo/>
     </Provider>
   </React.StrictMode>
 )
